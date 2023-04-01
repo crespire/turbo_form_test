807c9e557be7141254cf6b35711c4595ac7df3dc
crespire <hello@crespire.dev>
2023-04-01 08:12:52 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :posts
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get 'update_form', to: 'posts#update_form'
+  root "posts#index"
+
+end
